407fc3d6cd940638ba85d85513b67a5e67398ec7
Ivan <plastun.vanya@yandex.ru>
2019-02-20 12:26:48 +0300

    landing glow was ready

---
+++ app/js/main.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function() {
+   $("#carousel").owlCarousel( {
+       loop: true,
+       autoplay: true,
+       smartSpeed: 3000,
+       autoplayTimeout: 5000,
+       responsive: {
+           300: {
+               items: 1
+           },
+           
+           400: {
+               items: 1
+           },
+           1000: {
+               items: 1
+           }
+       }
+   });
+});
